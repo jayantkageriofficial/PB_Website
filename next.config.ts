6832e4be8596c55893eda74b7f0ac7630457ae22
Jayant Hegde Kageri <jayantkageri@gmail.com>
2026-08-09 15:41:31 +0530

    Add Next.js Rewrite for /pbctf

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  rewrites: async () => [
+    {
+      source: "/pbctf/:path*",
+      destination: "https://pbctf.pointblank.club/pbctf/:path*",
+    },
+  ],
   images: {
     remotePatterns: [
       {
